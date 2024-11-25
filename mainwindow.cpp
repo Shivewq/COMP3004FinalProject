232821ba50c8092b8a6a3822b6cc99c34c0178b6
Themike92 <michael.me28@gmail.com>
2024-11-24 21:57:08 -0500

    Added menu functionality and History tab

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,12 +5,16 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    //Setup UI
     ui->setupUi(this);
     ui->stackedWidget->setCurrentIndex(0); // Set to homepage
     connect(ui->button_profiles, &QPushButton::clicked, this, &MainWindow::on_button_profiles_clicked);
+
     //setup device
     device = new Device();
-    connect(device->charge,&Battery::editBattery,this,&MainWindow::on_editBattery);
+    connect(device->getBattery(),&Battery::editBattery,this,&MainWindow::on_editBattery);
+    connect(device->getBattery(),&Battery::editBattery,this,&MainWindow::on_editBattery);
+
     //setup app
     app = new App(device);
     app->activeUser = new User();
@@ -20,29 +24,50 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
 void MainWindow::on_editBattery(int value){
+    if(ui->battery->value() != 0){
+        ui->battery->setValue(value);
+    }
     ui->label_batteryValue->setText(QString::number(value));
 }
 
-
-void MainWindow::on_button_home_clicked()
+void MainWindow::showBatteryMsg()
 {
+    QMessageBox msgBox(this);
+    msgBox.setWindowTitle("LOW BATTERY WARNING");
+    msgBox.setText("Battery level is at 20%. Please charge soon.");
+    msgBox.setIcon(QMessageBox::Warning);
+    msgBox.setStandardButtons(QMessageBox::Close);
+
+    msgBox.exec();
 }
 
-
+// **MENU SELECTION CODE**
+void MainWindow::on_button_home_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
 
 void MainWindow::on_button_history_clicked()
 {
+    ui->stackedWidget->setCurrentIndex(3);
 }
 
+void MainWindow::on_button_measure_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
 
 void MainWindow::on_button_profiles_clicked()
 {
     ui->stackedWidget->setCurrentIndex(2);
 
+    /* Eric Test Code
     app->measure();
     app->calculateScan(0);
     Scan* scan = app->activeUser->getScan(0);
     app->calculateReadingGraph(scan->getPoints().at(1));
+    */
 }
 
