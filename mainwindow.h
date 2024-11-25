773c3e8c09d4d43a628fe224a881454c1f84f9f6
Michael Roy <117496089+themike92@users.noreply.github.com>
2024-11-24 22:33:28 -0500

    Revert "profile page functionality"

--- mainwindow.h
+++ mainwindow.h
@@ -2,13 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QVariant>
 #include <QMessageBox>
 #include "device.h"
 #include "app.h"
 #include "user.h"
-
-#include <QStringListModel>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -23,12 +20,6 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    void on_Add_User_clicked();
-    void on_Delete_User_clicked();
-    void on_Update_User_clicked();
-    void update_Active_User(const QString &text);
-
-
 private slots:
     void showBatteryMsg();
     //Menu Selection Options
@@ -37,19 +28,11 @@ private slots:
     void on_button_profiles_clicked();
     void on_button_measure_clicked();
 
-
-
-
 private:
     Ui::MainWindow *ui;
     Device * device;
     App* app;
-    QStringListModel* userModel;
-
 public slots:
     void on_editBattery(int value);
 };
-
-
-
 #endif // MAINWINDOW_H
--- user.cpp
+++ user.cpp
@@ -4,31 +4,10 @@ User::User(QObject* parent,QString name, int weight, int height):QObject(parent)
 
 }
 User::~User(){}
-
 void User::addScan(Scan* scan){
     measurements.push_back(scan);
-    qInfo() << "Scan: " << measurements.back() << measurements.last()->getDate().date();
-
+    qInfo() << "Scan:" << measurements.back() << measurements.last()->getDate().date();
    for(int num: measurements.last()->getPoints()){
        qInfo() << num;
    }
-}
-
-QString User::getName(){
-    return name;
-}
-
-
-void User::setName(QString newName){
-    name = newName;
-    qInfo()<<"new name set";
-}
-
-void User::setWeight(int newWeight){
-    weight = newWeight;
-    qInfo()<<"new weight set";
-}
-void User::setHeight(int newHeight){
-    height = newHeight;
-    qInfo()<<"new height set";
 }
